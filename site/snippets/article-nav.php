a2c3990eb7ccc942ef71561619dddc0d07ca641b
martinrisseeuw <hello@martinr.nl>
2017-05-09 09:35:19 +0200

    test for article nav

--- site/snippets/article-nav.php
+++ site/snippets/article-nav.php
@@ -1,14 +1,10 @@
-<?php if($page->hasPrev()): ?>
-  <?php snippet('article-nav-item', ['item' => $page->prev(), 'direction' => 'prev']) ?>
-<? else: ?>
-  <?php snippet('article-nav-item', ['item' => $pages->find('home')->children()->last(), 'direction' => 'prev']) ?>
-<?php endif ?>
 
-<?php if($page->hasNext()): ?>
+  <?php snippet('article-nav-item', ['item' => $page->prev(), 'direction' => 'prev']) ?>
+
+
+
   <?php snippet('article-nav-item', ['item' => $page->next(), 'direction' => 'next']) ?>
-<? else: ?>
-  <?php snippet('article-nav-item', ['item' => $pages->find('home')->children()->first(), 'direction' => 'next']) ?>
-<?php endif ?>
+
 
 
 <script type="text/javascript">
