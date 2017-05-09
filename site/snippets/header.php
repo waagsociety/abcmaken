e2dfe8036b19cc0b7dd0b02654dd675bbe44f7f0
martinrisseeuw <hello@martinr.nl>
2017-05-09 09:46:03 +0200

    updated typekit

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,7 +6,7 @@
   <title><?php echo $page->title()->html() ?> - <?php echo $site->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-  <script src="https://use.typekit.net/wkf2lia.js"></script>
+  <script src="https://use.typekit.net/zgm8ukn.js"></script>
   <script>try{Typekit.load({ async: false });}catch(e){}</script>
   <link rel="canonical" href="<?= $page->url() ?>">
   <link rel="alternate" hreflang="en" href="en" />
