3467b7c3dd426af458d7303abb257e7590ed22a3
martinrisseeuw <hello@martinr.nl>
2017-05-09 09:32:39 +0200

    updated content folder

--- site/snippets/article-nav-item.php
+++ site/snippets/article-nav-item.php
@@ -1,3 +1,3 @@
-<a href="<?php echo $item->url() ?>" class="article-nav <?= $direction ?>-article">
-  <span><?= $item->title() ?></span>
+<a href="<?php echo $item->url() ?>" class="article-nav <?php echo $direction ?>-article">
+  <span><?php echo $item->title() ?></span>
 </a>
